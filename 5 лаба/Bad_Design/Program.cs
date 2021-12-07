ba8c6294f820480461c267974979d3670ef72a4e
Max <62742586+DevilInTheFlesh@users.noreply.github.com>
2021-12-07 21:15:36 +0300

    Add files via upload

---
+++ 5 лаба/Bad_Design/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bad_Design
+{
+    /*public interface IEmployee
+    {
+        bool AddDetailsEmployee();
+    }
+
+    Фирма попросила сделать так, чтообы читались данные только для мясников и мы добавим этот метод в интерфейс
+    */
+
+    public interface IEmployee
+    {
+        bool AddDetailsEmployee();
+        string ShowEmployeeDetails(int id);
+    }
+    //Но теперь у нас и пекари будут показывать свои данные
+
+    public class Baker : IEmployee
+    {
+        public bool AddDetailsEmployee()
+        {
+            return true;
+        }
+
+        public string ShowEmployeeDetails(int emplId)
+        {
+            return "Bakery worker";
+        }
+    }
+
+    public class Butcher : IEmployee
+    {
+        public bool AddDetailsEmployee()
+        {
+            return true;
+        }
+
+        public string ShowEmployeeDetails(int emplId)
+        {
+            return "Butchery shop worker";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IEmployee> list = new List<IEmployee>();
+            list.Add(new Baker());
+            list.Add(new Butcher());
+
+            foreach (IEmployee emp in list)
+            {
+                Console.WriteLine(emp.ShowEmployeeDetails(999));
+            }
+        }
+    }
+}
---
+++ 5 лаба/Good_Design/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Good_Design
+{
+    //Разделим интерфейсы
+    public interface IEmployeeAdd
+    {
+        bool AddDetailsEmployee();
+    }
+
+    public interface IEmployeeShow
+    { 
+        string ShowEmployeeDetails(int id);
+    }
+   
+    public class Baker : IEmployeeAdd
+    {
+        public bool AddDetailsEmployee()
+        {
+            return true;
+        }
+
+    }
+
+    public class Butcher : IEmployeeAdd, IEmployeeShow
+    {
+        public bool AddDetailsEmployee()
+        {
+            return true;
+        }
+
+        public string ShowEmployeeDetails(int emplId)
+        {
+            return "Butchery shop worker";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Butcher butcher = new Butcher();
+            Console.WriteLine(butcher.ShowEmployeeDetails(999));
+        }
+    }
+}
